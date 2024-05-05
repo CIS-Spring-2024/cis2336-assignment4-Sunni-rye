0de3e3153767c77d68d4aea1eceae0e54aaf82cd
Sunni-rye <linda22039@gmail.com>
2024-05-05 17:59:37 -0500

    updated paths

--- assign4_Linda_Lopez-19387/BACKEND/nodeJS/JS/Contactscript.js
+++ assign4_Linda_Lopez-19387/BACKEND/nodeJS/JS/Contactscript.js
@@ -1,15 +1,15 @@
 window.addEventListener('focus', function(event) {
     // Check if the focused element is a text input field or textarea
     if (event.target.matches('input[type="text"], textarea')) {
-    // Add highlight class when focused
+    // Add highlight 
     event.target.classList.add('highlight');
     }
     }, true); 
-    // Remove highlight when focus is lost
+    // Remove highlight 
     window.addEventListener('blur', function(event) {
     // Check if the blurred element is a text input field or textarea
     if (event.target.matches('input[type="text"], textarea')) {
-    // Remove highlight class when focus is lost
+    // Remove highlight
     event.target.classList.remove('highlight');
     }
     }, true);
